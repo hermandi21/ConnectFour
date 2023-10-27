98bc4e2ab72f518b4b7e85dacfdb56e5557e6f19
hermandi21 <121821476+hermandi21@users.noreply.github.com>
2023-10-27 12:04:53 +0200

    Tui without tests

--- src/main/scala/de/htwg/se/ConnectFour/ConnectFour.scala
+++ src/main/scala/de/htwg/se/ConnectFour/ConnectFour.scala
@@ -1,10 +1,22 @@
 package de.htwg.se.ConnectFour
 
-import de.htwg.se.ConnectFour.model.Player
+import de.htwg.se.ConnectFour.model._
+//import de.htwg.se.ConnectFour.util._
+import de.htwg.se.ConnectFour.view.Tui
+import de.htwg.se.ConnectFour.view.numRows
+import de.htwg.se.ConnectFour.view.numCols
 
-object ConnectFour {
-  def main(args: Array[String]): Unit = {
-    val student = Player("Andreas")
-    println("Hello, " + student.name)
+
+def main(args: Array[String]): Unit = {
+  
+    println("Welcome to Connect Four!")
+    println("To play, enter the column number (1-7) to drop your token.")
+
+    val numRows = 6
+    val numCols = 7
+    val emptyField = Vector.fill(numRows)(Vector.fill(numCols)(" "))
+    val matchfield = MatchfieldModel(emptyField)
+    println(matchfield)
+
   }
-}
+  
--- src/main/scala/de/htwg/se/ConnectFour/view/Tui.scala
+++ src/main/scala/de/htwg/se/ConnectFour/view/Tui.scala
@@ -1,5 +1,35 @@
 package de.htwg.se.ConnectFour.view
 
-class Tui() {
-    
+import de.htwg.se.ConnectFour.model.MatchfieldModel
+
+val numRows = 6
+val numCols = 7
+
+class Tui(matchfield: MatchfieldModel[Any]) {
+
+  def displayMatchfield(): Unit = {
+    println(matchfield)
+  }
+
+  def displayMessage(message: String): Unit = {
+    println(message)
+  }
+
+  def getUserInput(): Int = {
+    try {
+      val numCols = matchfield.rows.headOption.map(_.size).getOrElse(0)
+      print(s"Enter the column (1-$numCols) to drop your token: ")
+      val input = scala.io.StdIn.readInt()
+      if (input >= 1 && input <= numCols) {
+        input
+      } else {
+        displayMessage(s"Invalid input. Please enter a column number (1-$numCols).")
+        getUserInput()
+      }
+    } catch {
+      case _: NumberFormatException =>
+        displayMessage(s"Invalid input. Please enter a column number (1-$numCols).")
+        getUserInput()
+    }
+  }
 }
