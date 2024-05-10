700349167b2f8798e6c838ede914833a888e3abf
hermandi21 <121821476+hermandi21@users.noreply.github.com>
2024-05-10 08:45:32 +0200

    Delete connectfour directory

--- connectfour/src/main/scala/model/Player.scala
+++
@@ -1,7 +0,0 @@
-package de.htwg.se.VierGewinnt.model
-
-import io.AnsiColor._
-
-case class Player(name: String, chip: Chip) {
-  override def toString: String = s"${chip.getColorCode}$name${RESET}"
-}
--- connectfour/src/main/scala/util/Observer.scala
+++
@@ -1,5 +0,0 @@
-package de.htwg.se.VierGewinnt.util
-
-trait Observer{
-    def update: Unit
-}
