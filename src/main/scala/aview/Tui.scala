f26badc905a8f23d4339c97af055629f90e8ed25
hermandi21 <4ndr34s01@gmail.com>
2024-05-16 14:11:21 +0200

    added StatePattern - GameState

--- src/main/scala/aview/Tui.scala
+++ src/main/scala/aview/Tui.scala
@@ -11,35 +11,47 @@ import scala.util.Try
 
 class Tui(controller: Controller) extends Observer :
   controller.add(this)
-  val size = controller.playground.size
 
   def run =
-    println("Welcome to 'Vier Gewinnt'\n $controller ")
+    println("Welcome to 'Vier Gewinnt'\n ")
+    prepareGameType()
     getInputAndPrintLoop()
+
+
+  def prepareGameType(): Unit = 
+    println("Please select one of the gametype you want to play. For default settings ('Player' vs. 'Player', grid size=7) press ENTER\n0:'Player vs. Player', 1:'Player vs. Bot', 2:'Bot vs. Bot' ")
+    val gameType = readLine()
+    gameType match 
+      case "" => controller.newGame(0,7)
+      case "0" | "1" => 
+        println("Type the grid size")
+        val size = readLine()
+        controller.newGame(gameType.toInt, size.toInt) // TODO:Try-Monad
+      case "2" => println("not supported yet")
+        prepareGameType()
+      case "q" => 
+      case _ => prepareGameType()
 
   def getInputAndPrintLoop(): Unit =
     val input = readLine
     input match {
       case "q" => //Exit
-      case "computer" => 
-        controller.changeEnemyStrategy(input)
-        println("changed Enemy player!")
-        getInputAndPrintLoop()
-      case "person" =>
-        controller.changeEnemyStrategy(input)
-        getInputAndPrintLoop()
       case x if x.toIntOption == None =>
         println("doesn't look like a number")
         getInputAndPrintLoop()
-      case x if x.toInt < 1 || x.toInt > size =>
-        println("wrong input, try a number from 1 to " + size)
+      case x if x.toInt < 1 || x.toInt > controller.playground.size =>
+        println("wrong input, try a number from 1 to " + controller.playground.size)
         getInputAndPrintLoop()
       case _ =>
         controller.insertChip(input.toInt - 1)
         getInputAndPrintLoop()
     }
 
-  override def update: Unit = println(controller.toString)
+  override def update: Unit = {
+    println(controller.toString + "\n")
+    controller.printState
+  }
+    
 
 
 
