042ad882d96a37ce455574bcd7ba0f08977342f7
hermandi21 <4ndr34s01@gmail.com>
2024-05-16 10:00:38 +0200

    added checkWin() method and implmenting strategy-pattern

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -9,6 +9,21 @@ class Controller(var playground: Playground) extends Observable :
 
   def insertChip(col: Int): Unit =    
     playground = playground.insertChip(col)
-    notifyObservers
+    
+  def checkFull():Unit = 
+    playground.grid.checkFull() match {
+      case true => println("Grid is completely full")
+      case false => println("Grid is not completely full")
+    }
+      
+  def changeEnemyStrategy(strat: String):Unit = 
+    playground = playground.setEnemyStrategy(strat)
+    
+  def checkWinner():Unit =
+    playground.grid.checkWin() match
+      case 0 => 
+      case 1 => println(" Red win's \n")
+      case 2 => println(" Yellow win's \n")
+    
 
   override def toString = playground.toString
