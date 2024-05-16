a5ee8b487e1b101a9624ee73672ce590baa91fee
hermandi21 <4ndr34s01@gmail.com>
2024-05-16 12:19:11 +0200

    small changes

--- src/main/scala/model/EnemyComputerStrategy.scala
+++ src/main/scala/model/EnemyComputerStrategy.scala
@@ -4,25 +4,26 @@ import de.htwg.se.VierGewinnt.util.EnemyStrategy
 import scala.util.Random
 
 case class EnemyComputerStrategy() extends EnemyStrategy {
-  override def insertChip(pg: Playground, col: Int): Playground = {
-    if (pg.getPosition(col) != -1) {
-      val temp = pg.copy(grid = pg.grid.replaceCell(pg.getPosition(col), col, Cell(pg.player(0).chip)), player = pg.player.reverse, enemStrat = pg.enemStrat)
-      if (!temp.grid.checkFull() && temp.grid.checkWin() == 0)
-        ComputerinsertChip(temp)
-      else
-        temp
-    } else {
-      pg.copy(error = "This column is full. Try another one.")
-    }
+  override def insertChip(pg:Playground, col: Int): Playground = {
+    if (pg.getPosition(col) != -1)
+      val temp = pg.copy(pg.grid.replaceCell(pg.getPosition(col), col, Cell(pg.player(0).chip)), pg.player.reverse, pg.enemStrat)
+      temp.grid.checkFull() match {
+        case false if (temp.grid.checkWin() == 0) => ComputerinsertChip(temp)
+        case _ => temp
+      }
+
+    else
+      pg.error = "This column is full try another one"
+      pg
   }
 
-  private def ComputerinsertChip(pg: Playground): Playground = {
-    var chosenCol = Random.between(0, pg.size)
+  def ComputerinsertChip(pg:Playground): Playground = {
+    var chosenCol = Random.between(0,pg.size)
 
-    while (pg.getPosition(chosenCol) != -1) {
-      chosenCol = Random.between(0, pg.size)
-    }
+    while
+      pg.getPosition(chosenCol) == -1
+    do chosenCol = Random.between(0,pg.size)
 
-    pg.copy(grid = pg.grid.replaceCell(pg.getPosition(chosenCol), chosenCol, Cell(pg.player(0).chip)), player = pg.player.reverse, enemStrat = pg.enemStrat)
+    pg.copy(pg.grid.replaceCell(pg.getPosition(chosenCol), chosenCol, Cell(pg.player(0).chip)), pg.player.reverse, pg.enemStrat)
   }
 }
