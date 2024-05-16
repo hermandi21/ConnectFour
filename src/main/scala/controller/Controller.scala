617d3e1d357c189006994b739a8d5e976f9927d7
hermandi21 <4ndr34s01@gmail.com>
2024-05-16 22:33:53 +0200

    solved compilation bug

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -4,54 +4,58 @@ package controller
 import model.{BotPlayer, Chip, EnemyComputerStrategy, EnemyPersonStrategy, Grid, HumanPlayer, Player, Playground}
 import util.Observable
 
-class Controller(var playground: Playground, var gameType: Int) extends Observable :
+class Controller(var playground: Playground, var gameType: Int)extends Observable :
   def this(size: Int = 7) = this(new Playground(7), 0)
 
   var gamestate: GameState = GameState()
   var player: List[Player] = List()
 
   def newGame(gameType: Int, size: Int): Unit =
-  playground = new Playground(size)
-  setupPlayers(gameType)
-  gamestate.changeState(PlayState())
-  notifyObservers
+    playground = new Playground(size)
+    setupPlayers(gameType)
+    gamestate.changeState(PlayState())
+    notifyObservers
 
-  def setupPlayers(gameType: Int) = 
+
+  private def setupPlayers(gameType: Int) =
     gameType match
       case 0 => playground = playground.setEnemyStrategy("person")
         player = List(HumanPlayer("Player 1", Chip.YELLOW), HumanPlayer("Player 2", Chip.RED))
       case 1 => playground = playground.setEnemyStrategy("bot")
         player = List(HumanPlayer("Player 1", Chip.YELLOW), BotPlayer("Bot 1", Chip.RED, EnemyComputerStrategy()))
 
-  def insertChip(col: Int): Unit =    
+  def insertChip(col: Int): Unit =
     playground = playground.insertChip(col)
     checkFull()
     checkWinner()
     notifyObservers
-    
-  def checkFull():Unit = 
+
+  def checkFull(): Unit =
     playground.grid.checkFull() match {
-      case true => println("Grid is completely full")
-      case false => println("Grid is not completely full")
-    }
-      
-  def changeEnemyStrategy(strat: String):Unit = 
-    playground = playground.setEnemyStrategy(strat)
-    
-  def checkWinner(): Unit =
-    playground.grid.checkWin() match {
-      case 0 =>
-      case 1 => {
-        println("Winner is Red")
-        gamestate.changeState(WinState())
-      }
-      case 2 => {
-        println("Winner is Yellow")
-        gamestate.changeState(WinState())
-      }
+      case true => gamestate.changeState(TieState())
+      case false => gamestate.changeState(PlayState())
     }
 
+  def changeEnemyStrategy(strat: String): Unit =
+    playground = playground.setEnemyStrategy(strat)
+
+  def checkWinner(): Unit = {
+  val result = playground.grid.checkWin()
+  result match {
+    case Some(0) => // Handle the case where nobody wins
+    case Some(1) => {
+      println("Winner is Red")
+      gamestate.changeState(WinState())
+    }
+    case Some(2) => {
+      println("Winner is Yellow")
+      gamestate.changeState(WinState())
+    }
+    case None => // Handle the case where the game is still ongoing
+  }
+}
+
+
   def printState: Unit = gamestate.displayState()
-    
 
   override def toString = playground.toString
