c007c49be702dadb18c352d68a83773cbdac2ac3
hermandi21 <4ndr34s01@gmail.com>
2024-06-05 18:34:21 +0200

    edited gitignore

--- src/main/scala/model/playerComponent/PlayerInterface.scala
+++ src/main/scala/model/playerComponent/PlayerInterface.scala
@@ -3,6 +3,6 @@ package model.playerComponent
 trait PlayerInterface:
     def getChip(): Chip
     def getName(): String
-    def getHumanPlayer(name: String, chip: Chip): PlayerInterface = new HumansPlayer(name, chip)
-    def getBotPlayer(name: String, chip: Chip): PlayerInterface = new BotPlayer(name, chip)
+    //def getHumanPlayer(name: String, chip: Chip): PlayerInterface = new HumanPlayer(name, chip)
+    //def getBotPlayer(name: String, chip: Chip): PlayerInterface = new BotPlayer(name, chip)
     
