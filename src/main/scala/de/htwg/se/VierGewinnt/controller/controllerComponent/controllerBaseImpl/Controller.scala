87a214a870846a88151e6165a22d84176d12b149
hermandi21 <4ndr34s01@gmail.com>
2024-06-27 20:14:08 +0200

    Controller for fileIO

--- src/main/scala/de/htwg/se/VierGewinnt/controller/controllerComponent/controllerBaseImpl/Controller.scala
+++ src/main/scala/de/htwg/se/VierGewinnt/controller/controllerComponent/controllerBaseImpl/Controller.scala
@@ -4,6 +4,7 @@ import com.google.inject.name.{Named, Names}
 import com.google.inject.{Guice, Inject, Key}
 import de.htwg.se.VierGewinnt.VierGewinntModule
 import de.htwg.se.VierGewinnt.controller.controllerComponent.ControllerInterface
+import de.htwg.se.VierGewinnt.model.fileIoComponent.FileIOInterface
 import de.htwg.se.VierGewinnt.model.gridComponent.GridInterface
 import de.htwg.se.VierGewinnt.model.gridComponent.gridBaseImpl.{Chip, Grid}
 import de.htwg.se.VierGewinnt.model.playerComponent.playerBaseImpl.{BotPlayer, HumanPlayer, Player}
@@ -13,13 +14,19 @@ import de.htwg.se.VierGewinnt.model.playgroundComponent.{PlaygroundInterface, pl
 import de.htwg.se.VierGewinnt.util.{Command, Move, Observable, UndoManager}
 
 class Controller @Inject()(@Named("DefaultPlayground") var playground: PlaygroundInterface, @Named("DefaultGameType") var gameType: Int) extends Observable with ControllerInterface :
-  /*def this(size: Int = 7) =
-    this(playgroundBaseImpl.PlaygroundPvP(new Grid(size), List(HumanPlayer("Player 1", Chip.YELLOW), playerBaseImpl.HumanPlayer("Player 2", Chip.RED))),
-      0)*/
 
   val injector = Guice.createInjector(new VierGewinntModule)
+  val fileIo = injector.getInstance(classOf[FileIOInterface])
 
   override def gridSize: Int = playground.size
+
+  override def load: Unit =
+    playground = fileIo.load
+    notifyObservers
+
+  override def save: Unit =
+    fileIo.save(playground)
+    notifyObservers
 
   override def setupGame(gameType: Int, size: Int): Unit =
     gameType match
@@ -33,14 +40,25 @@ class Controller @Inject()(@Named("DefaultPlayground") var playground: Playgroun
   val undoManager = new UndoManager[PlaygroundInterface]
 
   override def doAndPublish(doThis: Move => PlaygroundInterface, move: Move): Unit =
-    if (!gamestate.state.equals(WinState()))
+    if (gameNotDone)
       playground = doThis(move)
       notifyObservers
 
   override def doAndPublish(doThis: => PlaygroundInterface): Unit =
-    if (!gamestate.state.equals(WinState()))
+    if (gameNotDone)
       playground = doThis
       notifyObservers
+
+  override def restartGame: Unit =
+    gamestate.changeState(PrepareState())
+    notifyObservers
+
+  override def isPreparing: Boolean =
+    gamestate.state.equals(PrepareState())
+
+  override def gameNotDone: Boolean =
+    !(gamestate.state.equals(WinState()) || gamestate.state.equals(TieState()))
+
 
   override def undo: PlaygroundInterface = undoManager.undoStep(playground)
 
@@ -49,22 +67,22 @@ class Controller @Inject()(@Named("DefaultPlayground") var playground: Playgroun
   override def insChip(move: Move): PlaygroundInterface = {
     val temp = undoManager.doStep(playground, InsertChipCommand(move))
     checkWinner(temp)
-    checkFull()
+    checkFull(temp)
     temp
   }
 
-  override def checkFull(): Unit =
-    playground.grid.checkFull() match {
-      case true => if (!gamestate.state.equals(WinState())) { gamestate.changeState(TieState()) }
-      case false => if (!gamestate.state.equals(WinState())) { gamestate.changeState(PlayState()) }
-    }
+  override def checkFull(pg: PlaygroundInterface): Unit =
+    pg.grid.checkFull() match
+      case true => if (gameNotDone)
+        gamestate.changeState(TieState())
+      case false => if (gameNotDone)
+        gamestate.changeState(PlayState())
 
   override def checkWinner(pg: PlaygroundInterface): Unit =
-    pg.grid.checkWin() match {
+    pg.grid.checkWin() match
       case None =>
       case Some(num) => //1 == Red, 2 == Yellow
         gamestate.changeState(WinState())
-    }
 
   override def getChipColor(row: Int, col: Int): String =
     playground.grid.getCell(row, col).chip.getColorCode
@@ -72,5 +90,5 @@ class Controller @Inject()(@Named("DefaultPlayground") var playground: Playgroun
   override def printState: String = gamestate.displayState()
 
   override def playgroundState: String = playground.getStatus()
-  
+
   override def toString = playground.toString
