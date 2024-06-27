18680442e5592abad2a50102a614b4e345e078f8
hermandi21 <4ndr34s01@gmail.com>
2024-06-27 20:12:53 +0200

    renamed TUI

--- src/main/scala/de/htwg/se/VierGewinnt/aview/TUI.scala
+++ src/main/scala/de/htwg/se/VierGewinnt/aview/TUI.scala
@@ -1,17 +1,14 @@
 package de.htwg.se.VierGewinnt
 package aview
 
-
 import de.htwg.se.VierGewinnt.controller.controllerComponent.ControllerInterface
-import de.htwg.se.VierGewinnt.controller.controllerComponent.controllerBaseImpl.Controller
-
 import scala.io.StdIn.readLine
 import util.{Move, Observer}
 
 import scala.util.Try
 
 
-class Tui(controller: ControllerInterface)extends Observer :
+class TUI(controller: ControllerInterface)extends Observer :
   controller.add(this)
 
   def run =
@@ -22,26 +19,46 @@ class Tui(controller: ControllerInterface)extends Observer :
   def prepareGameType(): Unit =
     println("Please select one of the game type you want to play. For default settings ('Player vs Player', grid size=7) press ENTER\n0:'Player vs. Player', 1:'Player vs. Bot', 2:'Bot vs. Bot'")
     val gameType = readLine()
-    gameType match
-      case "" => controller.setupGame(0, 7)
-      case "0" | "1" =>
-        println("Type the grid size")
-        val size = readLine()
-        controller.setupGame(gameType.toInt, size.toInt)
-      case "2" => println("not supported yet")
-        prepareGameType()
-      case "q" => //Exit
-      case _   => prepareGameType()
+    if (controller.isPreparing) //If controller is not preparing anymore, skip this
+      gameType match
+        case "" => controller.setupGame(0, 7)
+        case "0" | "1" =>
+          println("Type the grid size")
+          val size = readLine()
+          controller.setupGame(gameType.toInt, size.toInt)
+        case "2" => println("not supported yet")
+          prepareGameType()
+        case "save" =>
+          controller.save
+          println("Game saved.")
+          prepareGameType()
+        case "load" =>
+          controller.load
+          println("Game loaded.")
+        case "q" => //Exit
+        case _   => prepareGameType()
 
   def getInputAndPrintLoop(): Unit =
     val input = readLine()
     input match {
       case "q" => //Exit
+      case "restart" =>
+        controller.restartGame
+        prepareGameType();
+        getInputAndPrintLoop()
       case "undo" =>
         controller.doAndPublish(controller.undo);
         getInputAndPrintLoop()
       case "redo" =>
         controller.doAndPublish(controller.redo);
+        getInputAndPrintLoop()
+      case "save" =>
+        controller.save
+        println("Game saved.")
+        getInputAndPrintLoop()
+      case "load" =>
+        controller.load
+        println("Game loaded.")
         getInputAndPrintLoop()
       case x if x.toIntOption == None =>
         println("doesn't look like a number")
